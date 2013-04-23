436592dbce4cf82abbb04da3b393621c9221b644
Dan Shaw <dshaw@dshaw.com>
2013-04-23 10:33:00 -0700

    some colorhexa love and debugging.

--- alltogether.js
+++ alltogether.js
@@ -1,160 +1,136 @@
-var 
-	five = require('johnny-five')
-	, leds
-	, board
-	, photo 		// photoresistor
-	, state = 0 	// current color (0 = r, 1 = g, 2 = b)
-	, duty = 255	// current duty cycle
-	, maxPhotoValue = 0 // highest photoresistor value
-	, maxHue = [ ]	// color values at time of highest photoresistor value
-	, timer			// looooooooop
-	, red
-	, green			
-	, blue
-	, ping 			// ultrasonic sensor
-	, idle 			// boolean: is the sensor taking a reading? YES? NO!?
-;
+var five = require('johnny-five')
+  , board = new five.Board()
+  , photo             // photoresistor
+  , ultrasonic        // ultrasonic sensor
+  , state = 0         // current color (0 = r, 1 = g, 2 = b)
+  , duty = 255        // current duty cycle
+  , maxPhotoValue = 0 // highest photoresistor value
+  , maxHue = []       // color values at time of highest photoresistor value
+  , timer             // looooooooop
+  , idle = true       // boolean: is the sensor taking a reading? YES? NO!?
+  , leds
+  , red
+  , green      
+  , blue
 
-board = new five.Board();
 
 board.on('ready', function() {
-	
-	red = new five.Led(9);
-	blue = new five.Led(10);
-	green = new five.Led(11);
-	leds = [ red, green, blue ];
-	
-	photo = new five.Sensor({
+  console.log('board ready')
 
-		pin : "A0"
-		, freq : 200
+  red = new five.Led(9)
+  blue = new five.Led(10)
+  green = new five.Led(11)
+  leds = [red, green, blue]
+  
+  photo = new five.Sensor({ pin: 'A0' , freq: 200 })
 
-	});
+  ultrasonic = new five.Ping(3)
 
-	ping = new five.Ping(3);
+  ultrasonic.on('read', function(err, data) {
+    //console.log('Ulrasonic sensor: %s', data)
+    if(data < 500) {
+      if(idle) {
+        red.brightness(0)
+        green.brightness(0)
+        blue.brightness(0)
+        getColor()
+        console.log('Ultrasonic sensor: object detected at %s', data)
+        console.log('Hold still!')
+      }
+    } else if (!idle) {
+      goIdle()
+      console.log('Ultrasonic sensor: idle...')
+    }
+  })
 
-	ping.on('read', function(err, dat) {
+  photo.on('read', function(err, raw) {
+    if (err) {
+      return console.log('>>> Read error: %s', err)
+    }
+    //console.log('Photo resistor: raw: %s, normalized: %s', raw, this.normalized)
+  })
 
-		if(dat < 400) {
+  /**
+   * Step through color shifting by one 
+   */
+  function stepColor() {
+    stateCheck()
+    colorCheck()
+    leds[state].brightness(duty)
+    leds[(state + 1) % 3].brightness(255 - duty)
+  }
 
-			if(idle) {
+  /**
+   * Check if the duty cycle for our current color
+   * is done (0) & if so, swap 'state' to next color
+   */
+  function stateCheck() {
+    //console.log('duty %s, state %s', duty, state)
+    if(--duty > 0) {
+      return false
+    }
+    duty = 255
 
-				red.brightness(0);
-				green.brightness(0);
-				blue.brightness(0);
+    if (state < 3) console.log('...wait for it...', state, maxHue)
 
-				getColor();
-				console.log(">>> Object detected");
-			}
-		}
-		else if(!idle) {
+    if(++state == 3) {
+      // done with scan
+      state = 0
+      var hex = rgbToHex(maxHue)
+      console.log('color sensed: #%s - %s', hex, 'http://colorhexa.com/'+hex)
+      goIdle()
+    }
+    return true
+  }
 
-			goIdle();
-			console.log(">>> Idle...");
-		}
+  /**
+   * Check the latest value of our light sensor & 
+   * compare to see if it's higher than 
+  */
+  function colorCheck() {
+    if(photo.value > maxPhotoValue) {
+      maxPhotoValue = photo.value
+      maxHue = [
+        red.value
+      , green.value
+      , blue.value
+      ]
+    }
+  }
 
-	});
+  /**
+   * Start a color scan!
+   */
+  function getColor() {
+    idle = false
+    timer = setInterval(stepColor, 10)
+  }
 
-	photo.on('read', function(err, dat) {
+  /**
+   * Do nothing...
+   */
+  function goIdle() {
+    state = 0
+    duty = 255
+    idle = true
+    maxPhotoValue = 0
+    leds[0].brightness(0)
+    leds[1].brightness(0)
+    leds[2].brightness(0)
+    maxHue = []
+    clearInterval(timer)
+  }
 
-		if(err) { return console.log(">>> Read error: %s", err); }
+  function componentToHex(color) {
+      var hex = color.toString(16)
+      return hex.length == 1 ? '0' + hex : hex
+  }
 
-		//console.log("Raw value: %s, normalized: %s", dat, this.normalized);
-	});
-
-	/**
-	 * Step through color shifting by one 
-	 */
-	function stepColor() {
-
-		stateCheck();
-		colorCheck();
-		leds[state].brightness(duty);
-		leds[(state + 1) % 3].brightness(255 - duty);
-	};
-
-	/**
-	 * Check if the duty cycle for our current color
-	 * is done (0) & if so, swap 'state' to next color
-	 */
-	function stateCheck() {
-
-		if(--duty > 0) {
-
-			return false;
-		}
-		duty = 255;
-		if(++state == 3) {
-
-			// done with scan
-			state = 0;
-			console.log(">>> %s", rgbToHex(maxHue));
-			goIdle();
-
-		}
-		return true;
-	};
-
-	/**
-	 * Check the latest value of our light sensor & 
-	 * compare to see if it's higher than 
-	*/
-	function colorCheck() {
-
-		if(photo.value > maxPhotoValue) {
-
-			maxPhotoValue = photo.value; 
-			maxHue = [
-
-				red.value
-				, green.value
-				, blue.value
-
-			];
-		}
-	};
-
-	/**
-	 * Start a color scan!
-	 */
-	function getColor() {
-
-		idle = false;
-		timer = setInterval(stepColor, 10)
-	};
-
-	/**
-	 * Do nothing...
-	 */
-	function goIdle() {
-
-		state = 0;
-		duty = 255;
-		idle = true;
-		maxPhotoValue = 0;
-		leds[0].brightness(0);
-		leds[1].brightness(0);
-		leds[2].brightness(0);
-
-		maxHue = [ ];
-
-		clearInterval(timer);
-	};
-
-	function componentToHex(c) {
-
-    	var hex = c.toString(16);
-    	return hex.length == 1 ? "0" + hex : hex;
-	};
-
-	function rgbToHex(vals) {
-
-    	return [
-    		"#"
-    		, componentToHex(vals[0])
-    		, componentToHex(vals[1])
-    		, componentToHex(vals[2])
-    	].join('');
-
-	};
-});
+  function rgbToHex(vals) {
+      return [
+        , componentToHex(vals[0])
+        , componentToHex(vals[1])
+        , componentToHex(vals[2])
+      ].join('')
+  }
+})
