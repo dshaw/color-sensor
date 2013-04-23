841d2f7006baff78b93e750c2b3811d33aa197b3
Dan Shaw <dshaw@dshaw.com>
2013-04-23 10:33:17 -0700

    debugging

--- somelight.js
+++ somelight.js
@@ -1,58 +1,52 @@
-var 
-	five = require('johnny-five')
-	, leds
-	, board
-	, photo
-	, state = 0
-	, duty = 255
-	, maxHue
-	, green
-	, blue
-	, red
-;
+var five = require('johnny-five')
+  , board = new five.Board()
+  , photo
+  , state = 0
+  , duty = 255
+  , maxHue
+  , red
+  , green
+  , blue
+  , leds = []
 
-board = new five.Board();
 
 board.on('ready', function() {
-	
+	console.log('ready', board)
+
 	red = new five.Led(9);
 	blue = new five.Led(10);
 	green = new five.Led(11);
 	leds = [ red, green, blue ];
 	
 	photo = new five.Sensor({
-
-		pin : "A0"
+		  pin : "A0"
 		, freq : 200
-
 	});
 
-	photo.on('read', function(err, dat) {
-
+	photo.on('read', function(err, raw) {
 		if(err) { return console.log(">>> Read error: %s", err); }
-
-		console.log("Raw value: %s, normalized: %s", dat, this.normalized);
+		console.log("Photo resistor: raw: %s, normalized: %s", raw, this.normalized);
 	});
+
+  function stateCheck() {
+    if(--duty > 0) {
+      return false;
+    }
+    duty = 255;
+    if(++state == 3) {
+
+      state = 0;
+    }
+    return true;
+  }
 
 	function stepColor() {
-
 		stateCheck();
 		leds[state].brightness(duty);
 		leds[(state + 1) % 3].brightness(255 - duty);
-	};	
-	function stateCheck() {
+	}
 
-		if(--duty > 0) {
 
-			return false;
-		}
-		duty = 255;
-		if(++state == 3) {
-
-			state = 0;
-		}
-		return true;
-	};
 
 	setInterval(stepColor, 10)
 });
